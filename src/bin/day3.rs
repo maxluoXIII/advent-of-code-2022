7cc08c73822d7e01b3ed9947dc79c8f2e87fc3e6
Maxiwell Luo <maxluoxiii@gmail.com>
2022-12-03 01:16:34 -0600

    Complete day 3 part 2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -5,21 +5,24 @@ fn main() {
     let file = File::open("data/day3-full.txt").expect("Could not find data file");
     let reader = BufReader::new(file);
 
+    let mut group = Vec::new();
     let mut sum = 0;
     for line in reader.lines() {
         let line = line.unwrap();
-        let first_compartment = &line[..line.len()/2];
-        let second_compartment = &line[line.len()/2..];
+        group.push(line);
 
-        for item in first_compartment.chars() {
-            if second_compartment.contains(item) {
-                sum += if item.is_lowercase() {
-                    item as u32 - 'a' as u32 + 1
-                } else {
-                    item as u32 -'A' as u32 + 27
-                };
-                break
+        if group.len() == 3 {
+            for item in group[0].chars() {
+                if group[1].contains(item) && group[2].contains(item) {
+                    sum += if item.is_lowercase() {
+                        item as u32 - 'a' as u32 + 1
+                    } else {
+                        item as u32 -'A' as u32 + 27
+                    };
+                    break
+                }
             }
+            group.clear();
         }
     }
 
