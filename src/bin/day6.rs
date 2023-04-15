c00263132a154ca68da3ab6dbbc1afd269e60415
Maxiwell Luo <maxluoxiii@gmail.com>
2023-04-15 13:37:46 -0500

    Finish day 6 part 1

---
+++ src/bin/day6.rs
@@ -0,0 +1,34 @@
+use std::{
+    collections::{HashSet},
+    fs::File,
+    io::{BufReader, Read},
+};
+
+const START_MARKER_LENGTH: usize = 4;
+
+fn get_start_marker_pos(datastream: Vec<u8>) -> Option<usize> {
+    let mut curr = datastream.iter();
+    let mut pos = 0;
+    loop {
+        let start = curr.clone();
+        let set = HashSet::<&u8>::from_iter(start.take(START_MARKER_LENGTH));
+        if set.len() == START_MARKER_LENGTH {
+            return Some(pos + 4);
+        }
+
+        if curr.next() == None {
+            break;
+        }
+        pos += 1;
+    }
+    return None;
+}
+
+fn main() {
+    let file = File::open("data/day6-full.txt").expect("Could not find data file");
+    let mut reader = BufReader::new(file);
+    let mut datastream = Vec::new();
+    reader.read_to_end(&mut datastream).expect("Error reading file");
+    let marker_pos = get_start_marker_pos(datastream);
+    println!("Start marker position: {:?}", marker_pos);
+}
